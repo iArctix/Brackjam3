fb2514c1ace2c782becd2e595e4395fd7f61572d
iArctix <connortodd.ct@gmail.com>
2023-02-19 02:11:44 +0000

    frgdtgd
    re

--- Assets/Scripts/TimeSystem.cs
+++ Assets/Scripts/TimeSystem.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+
 
 public class TimeSystem : MonoBehaviour
 {
